6747c0dec58ba3e254ed5d71d446cd134b840b34
eef-g <etan328@gmail.com>
2023-03-07 10:17:46 -0800

    Added Manager Creation & Destruction

--- headers/manager.h
+++ headers/manager.h
@@ -1,2 +1,15 @@
 #include <stdio.h>
+#include "dslib.h"
 
+typedef struct Manager
+{
+    Listptr free_list;
+    Listptr busy_list;
+    int size;
+} Manager, * Managerptr;
+
+
+Managerptr Manager_new(int size);
+void Manager_init(Managerptr self, int size);
+void Manager_allocate(void* data, int size, dataType type);
+void Manager_destroy(Managerptr self);
--- src/manager.c
+++ src/manager.c
@@ -1,3 +1,32 @@
 #include <stdio.h>
 #include "../headers/manager.h"
 
+Managerptr Manager_new(int size)
+{
+    Managerptr returnedPointer = (Managerptr)malloc(sizeof(Manager));
+    Manager_init(returnedPointer, size);
+    return returnedPointer;
+}
+
+void Manager_init(Managerptr self, int size)
+{
+    self->busy_list = List_new();
+    self->free_list = List_new();
+    self->size = size;
+}
+
+
+// TODO: Create the Manager_allocate() function here
+
+
+
+
+void Manager_destroy(Managerptr self)
+{
+    if(self)
+    {
+        List_destroy(self->busy_list);
+        List_destroy(self->free_list);
+        free(self);
+    }
+}
